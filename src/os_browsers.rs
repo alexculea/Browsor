bf35209f5fcc5743466b24f1886ab3c38f8faa0e
Alex Culea <16594531+alexculea@users.noreply.github.com>
2020-08-21 01:23:54 +0300

    Adds WIP Version Info WIP

--- src/os_browsers.rs
+++ src/os_browsers.rs
@@ -1,28 +1,12 @@
 use simple_error::SimpleResult as Result;
 mod winapi {
-
     pub use winapi::shared::minwindef::DWORD;
     pub use winapi::shared::windef::HICON;
     pub use winapi::um::shellapi::{SHGetFileInfoW, SHFILEINFOW, SHGFI_ICON, SHGFI_LARGEICON};
     pub use winapi::um::winbase::GetBinaryTypeW;
     pub use winapi::um::winver::{GetFileVersionInfoSizeW, GetFileVersionInfoW, VerQueryValueW};
-
-    STRUCT! {struct VS_FIXEDFILEINFO {
-        dwSignature: DWORD,
-        dwStrucVersion: DWORD,
-        dwFileVersionMS: DWORD,
-        dwFileVersionLS: DWORD,
-        dwProductVersionMS: DWORD,
-        dwProductVersionLS: DWORD,
-        dwFileFlagsMask: DWORD,
-        dwFileFlags: DWORD,
-        dwFileOS: DWORD,
-        dwFileType: DWORD,
-        dwFileSubtype: DWORD,
-        dwFileDateMS: DWORD,
-        dwFileDateLS: DWORD,
-      }
-    }
+    pub use winapi::um::errhandlingapi::GetLastError;
+    pub use winapi::um::winnls::GetUserDefaultUILanguage;
 }
 
 // https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#maximum-path-length-limitation
@@ -70,8 +54,8 @@ impl Default for BinaryType {
 pub struct VersionInfo {
     company_name: String,
     file_description: String,
-    file_version: String,
-    product_version: String,
+    product_version:  String,
+    product_name:  String,
     binary_type: BinaryType,
 }
 
@@ -88,7 +72,7 @@ pub fn read_system_browsers_sync() -> Result<Vec<Browser>> {
 
     for browser in list.iter_mut() {
         // let version = read_browser_exe_version_info(&[WINDOWS_LONG_PATH_PREFIX, &browser.exe_path].concat());
-        match read_browser_exe_version_info(&browser.exe_path) {
+        match read_browser_exe_info(&browser.exe_path) {
             Ok(version) => browser.version = version,
             Err(e) => println!(
                 "Error with reading browser info for {}. Reason: {}",
@@ -149,33 +133,72 @@ pub fn read_browser_info_from_reg_key(reg_path: &str) -> std::io::Result<Browser
     })
 }
 
-pub fn read_browser_exe_version_info(path: &str) -> Result<VersionInfo> {
-    const SCS_32BIT_BINARY: u32 = 0;
-    const SCS_64BIT_BINARY: u32 = 6;
-    const SCS_DOS_BINARY: u32 = 1;
-    const SCS_OS216_BINARY: u32 = 5;
-    const SCS_WOW_BINARY: u32 = 2;
-    let mut product_version = String::from("");
-    let file_path_wide = crate::util::str_to_wide(path);
-    let binary_type = unsafe {
-        let mut win_api_binary_type: u32 = 0;
-        if winapi::GetBinaryTypeW(file_path_wide.as_ptr(), &mut win_api_binary_type) < 1 {
-            bail!(
-                "Cannot read binary type with GetBinaryTypeW for file {}",
-                path
-            );
-        }
+pub fn read_browser_exe_info(path: &str) -> Result<VersionInfo> {
+    let mut returnValue = read_exe_version_info(path)?;
+    returnValue.binary_type = read_exe_arch(path)?;
 
-        match win_api_binary_type {
-            SCS_32BIT_BINARY | SCS_DOS_BINARY | SCS_WOW_BINARY => BinaryType::Bits32,
-            SCS_64BIT_BINARY => BinaryType::Bits64,
-            _ => BinaryType::None,
-        }
+    Ok(returnValue)
+}
+
+/// For the given `path` it returns the architecture of the 
+/// executable to be either 32 or 64 bits.
+pub fn read_exe_arch(path: &str) -> Result<BinaryType> {
+    // WinAPI rust crate is missing the SCS_ constants thus
+    // we need to define the values here
+    // https://github.com/retep998/winapi-rs/issues/930
+    #[repr(u32)]
+    #[derive(Clone, Copy)]
+    enum WinApiBinaryType {
+        None = 12345678,
+        Bits32 = 0,
+        Bits64 = 6,
+        Dos = 1,
+        Wow = 2
+    }
+
+    let file_path_wide = crate::util::str_to_wide(path);    
+    let win_api_binary_type: WinApiBinaryType = WinApiBinaryType::None;
+    let api_call_result = unsafe { 
+        winapi::GetBinaryTypeW(file_path_wide.as_ptr(), &mut (win_api_binary_type as u32) as *mut u32)
     };
 
-    let mut dword: u32 = 0;
+    if api_call_result < 1 {
+        bail!(
+            "Cannot read binary type with GetBinaryTypeW for file {}",
+            path
+        );
+    }
+
+
+    Ok(match win_api_binary_type {
+        WinApiBinaryType::Bits32 | WinApiBinaryType::Dos | WinApiBinaryType::Wow => BinaryType::Bits32,
+        WinApiBinaryType::Bits64 => BinaryType::Bits64,
+        _ => BinaryType::None,
+    })   
+}
+
+/// Reads certain file attributes specific to Windows executables as per the fields
+/// in `VersionInfo` struct based on the given `path`
+///
+/// The fields read are:
+/// - ProductName
+/// - CompanyName
+/// - ProductVersion
+///
+/// ### Implementation details
+/// The implementation is overly complicated due to the goal of having a correct
+/// implementation as per Microsoft Docs, thus what we do is:
+///  - ask the OS what size is needed to hold the whole blob containing the file version fields
+///  - allocate a buffer with that size and ask the OS to copy the hole blob in our buffer
+///  - the blob contains some Windows specific hierarchy structures where the data we're interested in is all beneath a certain language code
+///  - we ask the OS to tell us what languages are there in the .exe file
+///  - we ask the OS what is the OS setting for the user's language and we pick .exe language that matches the UI default or the language neutral entry which Windows defines it as a lang code of 0, or the first element found
+///  - we ask the for specific values of the properties `ProductName`, `CompanyName`, `ProductVersion` and if they're not  `UTF-16` we convert them based on the indicated `Code Page`.
+pub fn read_exe_version_info(path: &str) -> Result<VersionInfo> {
+    const UTF16_WINDOWS_CODE_PAGE: u16 = 1200; 
+    let file_path_wide = crate::util::str_to_wide(path);
     let file_version_size: u32 =
-        unsafe { winapi::GetFileVersionInfoSizeW(file_path_wide.as_ptr(), &mut dword) };
+        unsafe { winapi::GetFileVersionInfoSizeW(file_path_wide.as_ptr(), &mut 0) };
     if file_version_size == 0 {
         bail!(
             "Cannot read file version size with GetFileVersionInfoSizeExW for {}",
@@ -184,51 +207,130 @@ pub fn read_browser_exe_version_info(path: &str) -> Result<VersionInfo> {
     }
 
     unsafe {
-        let mut buffer: Vec<u16> = Vec::with_capacity(file_version_size as usize);
+        let mut version_info_blob: Vec<u8> = Vec::with_capacity(file_version_size as usize);
         if winapi::GetFileVersionInfoW(
             file_path_wide.as_ptr(),
             0,
             file_version_size,
-            buffer.as_mut_ptr() as *mut std::ffi::c_void,
+            version_info_blob.as_mut_ptr() as *mut std::ffi::c_void,
         ) == 0
         {
             bail!(
-                "Cannot read file version size with GetFileVersionInfoExW for {}",
+                "Cannot get file version info data with GetFileVersionInfoW for {}",
                 path
             );
         }
 
-        // pointer holding mem position within buffer above to the requested info
-        let mut buffer_pointer: *mut std::vec::Vec<u8> = std::ptr::null_mut();
-        let mut size = 0;
-        let version_info_prop = crate::util::str_to_wide("\\");
-        // ToDO: Fix VerQueryValue call
-        // Issue: GetFileVersionInfo above appears to be returning correctly
-        // Check that:
-        // -- buffer above is filled with data
-        // -- why buffer_pointer is left as NULL by VerQueryValue instead of being filled with the correct info
-        let result = winapi::VerQueryValueW(
-            buffer.as_mut_ptr() as *mut std::ffi::c_void,
-            version_info_prop.as_ptr(),
-            &mut (buffer_pointer as *mut std::ffi::c_void),
-            &mut size,
-        );
-        if result > 0 && size > 0 && buffer_pointer != std::ptr::null_mut() {
-            let mut prd_ver: Vec<u8> = Vec::with_capacity(size as usize);
-            // std::ptr::copy(buffer_pointer, prd_ver.as_mut_ptr() as *mut std::ffi::c_void, size as usize);
-            let vs_fixed_file_info: winapi::VS_FIXEDFILEINFO =
-                std::mem::transmute_copy(&buffer_pointer);
-            let string_raw: &str = std::mem::transmute_copy(&buffer_pointer);
+        #[repr(C)]
+        #[derive(Debug)]
+        struct LANGANDCODEPAGE {
+            wLanguage: u16,
+            wCodePage: u16
+        };
+        type PCLANGANDCODEPAGE = *const LANGANDCODEPAGE;
 
-            println!("{:?}", string_raw);
+        // pointer within `buffer` var above based on the sub block given to VerQueryValueW
+        let mut out_pointer = std::ptr::null_mut();
+        
+        // the number of bytes VerQueryValueW has written for the the requested sub block from within the `version_info_blob`
+        let mut out_size: u32 = 0;
+        
+        let translations_sub_block = crate::util::str_to_wide("\\VarFileInfo\\Translation");
+
+        let result = winapi::VerQueryValueW(
+            version_info_blob.as_ptr() as *mut std::ffi::c_void,
+            translations_sub_block.as_ptr(),
+            &mut out_pointer,
+            &mut out_size,
+        );
+
+        println!("Address of the verinfo buffer: {:p}", &version_info_blob);
+        println!("Address of the VerQueryValue pointer: {:p}", out_pointer);
+        let raw_buff = std::slice::from_raw_parts::<u8>(out_pointer as *const u8, out_size as usize);
+        println!("Raw buffer:\n{:?}", raw_buff);
+
+        if result == 0 || out_size == 0 || out_pointer == std::ptr::null_mut() {
+            bail!("Failed to read version info for {}. GetLastError: {:#x}", path, winapi::GetLastError());
+        }
+
+        let translations_len = out_size as usize / std::mem::size_of::<LANGANDCODEPAGE>();
+        
+        // TODO: do we need to forget this because it's technically part of the version_info_blob?
+        let translations: &[LANGANDCODEPAGE] = std::slice::from_raw_parts(out_pointer as PCLANGANDCODEPAGE, translations_len);
+
+        let user_lang_id = winapi::GetUserDefaultUILanguage();
+        let default_lang_id = 0; // 0 means language neutral
+
+        // look at the translations list and find the one matching
+        // the OS language (user_lang_id) or find the language neutral one (default_lang_id)
+        // or just return the first element (&translations[0])
+        let translation: &LANGANDCODEPAGE = translations
+            .iter()
+            .find(|item| item.wLanguage == user_lang_id)
+            .unwrap_or_else(|| translations
+                .iter()
+                .find(| item | item.wLanguage == default_lang_id)
+                .unwrap_or_else(|| &translations[0])
+            );
+
+        let base_block = format!("\\StringFileInfo\\{:04x}{:04x}", translation.wLanguage, translation.wCodePage);
+        let product_name_block = base_block.clone() + "\\ProductName";
+        let company_name_block = base_block.clone() + "\\CompanyName";
+        let product_version_block = base_block.clone() + "\\ProductVersion";
+
+        let mut results = Vec::<String>::with_capacity(3);
+
+        for block in [
+            &product_name_block,
+            &company_name_block,
+            &product_version_block
+        ].into_iter() {
+            // pointer within `buffer` var above based on the sub block given to VerQueryValueW
+            let mut out_pointer = std::ptr::null_mut();
+        
+            // the number of bytes VerQueryValueW has written for the the requested sub block from within the `version_info_blob`
+            let mut out_size: u32 = 0;      
+            let result = winapi::VerQueryValueW(
+                version_info_blob.as_ptr() as *mut std::ffi::c_void,
+                crate::util::str_to_wide(block).as_ptr(),
+                &mut out_pointer,
+                &mut out_size,
+            );
+
+            if result == 0 || out_size == 0 || out_pointer == std::ptr::null_mut() {
+                results.push(String::from(""));
+                continue;
+            }
+
+            let mut raw_wide_string: Vec<u16> = vec!(0);
+            
+            if translation.wCodePage != UTF16_WINDOWS_CODE_PAGE {
+                // TODO: do we need to forget this because it's technically part of the version_info_blob?
+                let raw_string = std::slice::from_raw_parts(out_pointer as *const i8, out_size as usize).to_vec();
+                raw_wide_string = crate::util::ansi_str_to_wide(&raw_string, translation.wCodePage)?;
+            } else {
+                raw_wide_string = std::slice::from_raw_parts(out_pointer as *const u16, out_size as usize).to_vec();
+            }
+
+            let result_str = crate::util::wide_to_str(&raw_wide_string);       
+            results.push(result_str);
+        }
+
+        if let [
+            product_name, 
+            company_name,
+            product_version,
+        ] = results.as_slice() {
+            return Ok(VersionInfo {
+                product_name: product_name.into(),
+                product_version: product_version.into(),
+                company_name: company_name.into(),
+                ..Default::default()
+            })
+        } else {
+            bail!("Not all required props were found.");
         }
     }
-
-    Ok(VersionInfo {
-        binary_type,
-        product_version,
-        ..Default::default()
-    })
 }
 
 pub fn get_exe_file_icon(path: &str) -> Result<winapi::HICON> {
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
+use simple_error::SimpleResult as Result;
 use raw_window_handle::HasRawWindowHandle;
 use winapi::winrt::roapi::RoInitialize;
+use winapi::um::stringapiset::MultiByteToWideChar;
 
 pub unsafe fn initialize_runtime_com() -> winrt::Result<()> {
   let result = winrt::ErrorCode::from(Ok(RoInitialize(
@@ -41,6 +43,35 @@ pub fn str_to_wide(string: &str) -> Vec<u16> {
 
 pub fn wide_to_str(buf: &Vec<u16>) -> String {
   String::from_utf16_lossy(&buf)
+}
+
+/// From the given buffer `src_string` use the Windows API to convert the
+/// ANSI string with the given `code_page`.
+///
+/// [MSDN Info](https://docs.microsoft.com/en-us/windows/win32/intl/code-pages)
+pub fn ansi_str_to_wide(src_string: &Vec<i8>, code_page: u16) -> Result<Vec<u16>> {
+  let receiving_capacity = src_string.len() * 2;
+  // generally single byte strings use 1 byte per character
+  // we allocate twice of that hoping we won't get truncated
+
+  let mut dst_string = Vec::<u16>::with_capacity(receiving_capacity);
+  let result = unsafe { MultiByteToWideChar(
+    code_page as u32, 
+    0, 
+    src_string.as_ptr() as *const i8,
+    src_string.len() as i32, // size in bytes is the same as the length
+    dst_string.as_mut_ptr(), 
+    (receiving_capacity - 1) as i32
+  ) };
+  // this call is safe as long as we trust WinAPI
+  // to respect the indicated capacities in cbMultiByte (param 4)
+  // and in cchWideChar (param 6)
+
+  if result == 0 {
+    bail!("Could not convert the given string. Call GetLastError from WinAPI to find out why.");
+  }
+
+  Ok(dst_string)
 }
 
 pub fn as_u8_slice(v: &[u32]) -> &[u8] {
